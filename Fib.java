5ac9fb006521359a9eb4bd2763b7821946a3628f
Shree0404 <shrinivasuravakonda@gmail.com>
2025-05-13 16:21:41 +0530

    Add files via upload

---
+++ Fib.java
@@ -0,0 +1,20 @@
+package Operations;
+
+public class Fib {
+    public static void main(String[] args) {
+        int n = 10;  
+        System.out.println("Fibonacci Series up to " + n + " terms:");
+
+        int first = 0, second = 1;
+        System.out.print(first + " " + second + " "); 
+
+        for (int i = 3; i <= n; i++) {
+            int next = first + second;
+            System.out.print(next + " ");
+            first = second; 
+            second = next;
+        }
+    }
+} 
+    
+
